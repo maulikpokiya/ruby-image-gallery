5549b33e9a1ee6be051e55336874473f95445f4b
maulikpokiya <maulik.pokiya@gmail.com>
2014-01-07 12:47:29 -0500

    init

---
+++ public/application.js
@@ -0,0 +1,13 @@
+$(".forms").hide(); // hide both forms at start
+$("ul li a").click(function(){
+	var idToShow = $(this).attr("href");
+	$(idToShow).show().siblings(".forms").hide();
+	return false;	// do not navigate to the beginning of form
+});
+$("#sign_up_confirmation").keyup(function(){
+	if( $(this).val() != $("#sign_up_password").val() ) {
+		$(this).addClass("error").next().text("Passwords don't match.");	// will go to span
+	} else {
+		$(this).removeClass("error").next().text("");				
+	}
+});
